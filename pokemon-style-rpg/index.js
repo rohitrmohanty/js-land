d725f931ec04c4768f776a5738650501c396c3fe
Rohit R. Mohanty <rohit.mohanty@nextuple.com>
2023-08-18 10:47:54 +0530

    Sprite class added for bg and movement added for player sprite

--- pokemon-style-rpg/index.js
+++ pokemon-style-rpg/index.js
@@ -18,19 +18,96 @@ console.log(image);
 const playerImage = new Image();
 playerImage.src = "./assets/images/playerDown.png";
 
-image.onload = () => {
-  c.drawImage(image, -700, -550);
-  playerImage.onload = () => {
-    c.drawImage(
-      playerImage,
-      0,
-      0,
-      playerImage.width / 4,
-      playerImage.height,
-      canvas.width / 2 + playerImage.width / 16,
-      (canvas.height - playerImage.height) / 2,
-      playerImage.width / 4,
-      playerImage.height
-    );
+class Sprite {
+  constructor({ position, image, velocity }) {
+    this.position = position;
+    this.image = image
+  }
+
+  draw() {
+    c.drawImage(this.image, this.position.x, this.position.y);
   }
 }
+
+const background = new Sprite({
+  position: {
+    x: -700,
+    y: -550
+  },
+  image: image
+});
+
+const keys = {
+  w: { pressed: false },
+  a: { pressed: false },
+  s: { pressed: false },
+  d: { pressed: false }
+}
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  background.draw();
+  c.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas.width / 2 + playerImage.width / 16,
+    (canvas.height + playerImage.height) / 2,
+    playerImage.width / 4,
+    playerImage.height
+  );
+
+  if (keys.w.pressed && lastKey === 'w') background.position.y += 3;
+  else if (keys.s.pressed && lastKey === 's') background.position.y -= 3;
+  else if (keys.a.pressed && lastKey === 'a') background.position.x += 3
+  else if (keys.d.pressed && lastKey === 'd') background.position.x -= 3;
+}
+animate();
+
+let lastKey = '';
+console.log({ lastKey });
+window.addEventListener('keydown', (e) => {
+  console.log(`Why are you pressing the ${e.key} key bro?`);
+  switch (e.key) {
+    case 'w':
+      keys.w.pressed = true;
+      lastKey = 'w';
+      break;
+    case 'a':
+      keys.a.pressed = true;
+      lastKey = 'a';
+      break;
+    case 's':
+      keys.s.pressed = true;
+      lastKey = 's';
+      break;
+    case 'd':
+      keys.d.pressed = true;
+      lastKey = 'd';
+      break;
+    default:
+      return
+  }
+});
+
+window.addEventListener('keyup', (e) => {
+  console.log(`Why are you leaving the ${e.key} key bro?`)
+  switch (e.key) {
+    case 'w':
+      keys.w.pressed = false;
+      break;
+    case 'a':
+      keys.a.pressed = false;
+      break;
+    case 's':
+      keys.s.pressed = false;
+      break;
+    case 'd':
+      keys.d.pressed = false;
+      break;
+    default:
+      break;
+  }
+});
