b66762c8e6f0826189a06ad3fec4df4e454293b3
Rohit R. Mohanty <rohit.mohanty@nextuple.com>
2023-08-19 18:20:43 +0530

    Created collisionsMap

--- pokemon-style-rpg/index.js
+++ pokemon-style-rpg/index.js
@@ -5,11 +5,38 @@ const c = canvas.getContext('2d');
 console.log(canvas);
 console.log(c);
 
+try {
+    console.log('Collisions Data has been imported successfully!', { collisions });
+} catch (error) {
+  console.log(error);  
+}
+
+const collisionsMap = [];
+for (let index = 0; index < collisions.length; index+=70) {
+  collisionsMap[index] = collisions.slice(index * 70, index * 70 + 70);
+}
+
+try {
+    if (collisionsMap.length > 0) console.log('CollisionsMap has been generated successfully!', { collisionsMap });
+} catch (error) {
+  console.log(error);  
+}
+
 canvas.width = 1024;
 canvas.height = 576;
 
-c.fillStyle = 'white';
-c.fillRect(0, 0, canvas.width, canvas.height); 
+class Boundary {
+  constructor({ position }) {
+    this.position = position;
+    this.width = 48;
+    this.height = 48;
+  }
+
+  draw() {
+    c.fillStyle = 'red';
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
 
 const image = new Image();
 image.src = "./assets/pellet_town.png";
